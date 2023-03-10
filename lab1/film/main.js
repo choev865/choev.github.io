98ad47711b3389c601f4f8dbd68a0bfcd8ce6e04
choev865 <choev865@newschool.edu>
2023-03-10 02:14:53 -0500

    Update main.js

--- lab1/film/main.js
+++ lab1/film/main.js
@@ -1,32 +1,3 @@
-/*var spotlightImage = document.getElementById("spotlight-image");
-var backButton = document.getElementById("back-button");
-var nextButton = document.getElementById("next-button");
-
-var imageSources = ["./scenes/one.png", "./scenes/two.png", "./scenes/three.png",
- "./scenes/four.png"];
-var currentPosition = 0;
-
-backButton.addEventListener("click", function() {
-  if (currentPosition > 0) {
-    currentPosition--;
-    spotlightImage.src = imageSources[currentPosition];
-  }
-});
-
-nextButton.addEventListener("click", function() {
-  if (currentPosition < imageSources.length - 1) {
-    currentPosition++;
-    spotlightImage.src = imageSources[currentPosition];
-  }
-});
-
-
-
-
-*/
-
-
-
 
 const filmstripItems = document.querySelector(".filmstrip-items");
 const prevBtn = document.querySelector(".filmstrip-prev-btn");
